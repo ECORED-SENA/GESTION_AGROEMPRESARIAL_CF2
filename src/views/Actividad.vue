c03ff9b4efc5af4c0d4421c3738acb2ec416767d
semava1713 <smvalencia024@misena.edu.co>
2022-09-12 12:30:41 -0500

    correciones
    correciones

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -17,7 +17,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Descripción de la actividad didáctica.
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/CF2AD2/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
@@ -35,7 +35,7 @@
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Descripción de la actividad didáctica.
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/CF2AD1/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
